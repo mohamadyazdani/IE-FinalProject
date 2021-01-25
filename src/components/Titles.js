6e4dc37f6a6bf5a0008dc9ba91971e05a9626f0d
mohamadyazdani <68750208+mohamadyazdani@users.noreply.github.com>
2021-01-25 11:27:06 +0330

    Add files via upload

---
+++ src/components/Titles.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Weather Forecast ................</h1>
+		<h3 className="title-container__subtitle">By Yazdani</h3>
+		<h3 className="title-container__subtitle"> and Asadi</h3>
+	</div>
+);
+
+export default Titles;
